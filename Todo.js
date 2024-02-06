83f3a9a7371dacf5e04edcf48e62f9d35d1f4e4b
ignitetechtest <144653630+ignitetechtest@users.noreply.github.com>
2024-02-06 11:03:51 +0530

    Create Todo.js

---
+++ Todo.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() === '') return;
+    setTodos([...todos, inputValue]);
+    setInputValue('');
+  };
+
+  const handleRemoveTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
